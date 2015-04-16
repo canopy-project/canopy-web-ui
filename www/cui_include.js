108a4f49adae167f0930a29628f4c4279d21a2a3
Greg Prisament <greg@canopy.link>
2015-04-15 22:36:39 -0700

    Simplify script imports

---
+++ www/cui_include.js
@@ -0,0 +1,4 @@
+document.write('<script src=' + CUI_INCLUDE_PATH + '/cui_node.js type="text/javascript"><\/script>');
+document.write('<script src=' + CUI_INCLUDE_PATH + '/nodes/generic/cui_switcher.js type="text/javascript"><\/script>');
+document.write('<script src=' + CUI_INCLUDE_PATH + '/nodes/test/cui_hello.js type="text/javascript"><\/script>');
+document.write('<script src=' + CUI_INCLUDE_PATH + '/nodes/test/cui_hello_list.js type="text/javascript"><\/script>');
