3828a78f708f442ef25eac41972adab6e00c71d2
Greg Prisament <greg@canopy.link>
2015-05-08 23:28:52 -0700

    Fix release package script

--- create_src_pkg.sh
+++ create_src_pkg.sh
@@ -4,7 +4,7 @@ if [[ -z $1 ]]; then
     exit
 fi
 mkdir canopy-web-ui-$1
-cp *.js *.txt *.html canopy-device-mgr-$1
+cp LICENSE README.md canopy-web-ui-$1
 cp -r www canopy-web-ui-$1
 tar -czvf canopy-web-ui_${1}.src.tar.gz canopy-web-ui-$1
 rm -r canopy-web-ui-$1
